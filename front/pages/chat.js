bb6eb996d8198ee9e7d8a9b1db667a23434845dd
minzai0116 <63540495+minzai0116@users.noreply.github.com>
2025-04-21 19:22:46 +0900

    ForgotPage added
    LoginPage + ChatPage edited

--- front/pages/chat.js
+++ front/pages/chat.js
@@ -1,31 +1,3 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import Sidebar from "../components/Sidebar";
-import ChatWindow from "../components/ChatWindow";
-import styles from "../styles/ChatPage.module.css";
-
-export default function ChatPage() {
-    const router = useRouter();
-    const [isLoading, setIsLoading] = useState(true);
-    const [isGuest, setIsGuest] = useState(false);
-
-    useEffect(() => {
-        const user = JSON.parse(localStorage.getItem("user"));
-
-        if (!user) {
-            router.push("/login");
-        } else {
-            setIsGuest(!!user.guest);
-            setIsLoading(false);
-        }
-    }, []);
-
-    if (isLoading) return <div>Loading...</div>;
-
-    return (
-        <div className={styles.chatPage}>
-            <Sidebar isGuest={isGuest} />
-            <ChatWindow isGuest={isGuest} />
-        </div>
-    );
-}
+import dynamic from "next/dynamic";
+const ChatPage = dynamic(() => import("../components/ChatPage"), { ssr: false });
+export default ChatPage;
