7c42723d97f6da25e7a4233ff77b4207b88124d5
minzai0116 <minjae011653@gmail.com>
2025-05-08 19:45:17 +0900

    refactor#83 : 커뮤니티 레이아웃, 챗 출력 레이아웃 수정

--- front/pages/api/title.js
+++ front/pages/api/title.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
 다음은 사용자와 상담사의 대화입니다.
 이 대화의 핵심을 담은 짧고 자연스러운 제목을 만들어주세요.
 형식은 오직 "제목"만 포함하고, 불필요한 설명은 제거해주세요.
-${content}
+${cleanedContent}
 `;
 
         const response = await openai.chat.completions.create({
